a124d7afb2b69f0f1587f99cf83036e34cad02ca
EmilienDupont <emilien.dupont@ymail.com>
2018-01-24 21:28:50 -0800

    :banana: added fashion mnist dataset

--- dataloaders.py
+++ dataloaders.py
@@ -20,6 +20,24 @@ def get_mnist_dataloaders(batch_size=128):
     return train_loader, test_loader
 
 
+def get_fashion_mnist_dataloaders(batch_size=128):
+    """Fashion MNIST dataloader with (32, 32) sized images."""
+    # Resize images so they are a power of 2
+    all_transforms = transforms.Compose([
+        transforms.Resize(32),
+        transforms.ToTensor()
+    ])
+    # Get train and test data
+    train_data = datasets.FashionMNIST('../fashion_data', train=True, download=True,
+                                       transform=all_transforms)
+    test_data = datasets.FashionMNIST('../fashion_data', train=False,
+                                      transform=all_transforms)
+    # Create dataloaders
+    train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=True)
+    return train_loader, test_loader
+
+
 def get_lsun_dataloader(path_to_data='../lsun', dataset='bedroom_train',
                         batch_size=64):
     """LSUN dataloader with (128, 128) sized images.
